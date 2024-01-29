04c78487be9478a56826f75fdac7e035a6a10db0
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2024-01-29 08:34:10 +0200

    change admin url

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('egipt')
             ->login()
             ->defaultThemeMode(ThemeMode::Dark)
             ->colors([

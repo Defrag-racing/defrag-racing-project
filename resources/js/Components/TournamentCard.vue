f8ca993924c20aff76d2ba851fdf3ebd1c77a707
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2024-04-09 13:35:13 +0200

    add socials to profile, tournament not published tag

--- resources/js/Components/TournamentCard.vue
+++ resources/js/Components/TournamentCard.vue
@@ -20,7 +20,13 @@
 
 <template>
     <div class="w-full">
-        <Link :href="route('tournaments.show', tournament.id)" class="w-full flex items-end mb-4 mx-auto border border-grayop-500 border-4 rounded-lg" :style="`min-height: 400px; width: 600px; background-size: cover; background-image: url(/storage/${tournament.image});`">
+        <Link :href="route('tournaments.show', tournament.id)" class="flex flex-col justify-between mb-4 mx-auto border border-grayop-500 border-4 rounded-lg" :style="`height: 400px; width: 600px; max-width: 100%; background-size: cover; background-image: url(/storage/${tournament.image});`">
+            <div class="flex justify-end">
+                <div v-if="! tournament.published" :class="`rounded-md bg-blackop-30 p-2 uppercase text-white font-bold mr-3 mt-2`">
+                    Not Published
+                </div>
+            </div>
+
             <div class="h-24 w-full px-5 py-5 rounded-b-lg" style="background-color: #000000b9;">
                 <div class="text-2xl font-bold hover:underline text-white">{{ tournament.name }}</div>
     
